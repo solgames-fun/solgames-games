fe72f41961965c3d28c6cd7e6f9eee69d57e1328
Trifan Grigore-Gabriel <trifangaby71@protonmail.com>
2021-10-09 21:42:25 +0300

    ✨ Early Bunny Game implementation

---
+++ src/bunny_runner/index.jsx
@@ -0,0 +1,57 @@
+import "./styles.scss";
+import React, { useEffect, useState } from "react";
+const BunnyRunner = () => {
+    const [time, setTime] = useState(0);
+    var timing = React.useRef();
+
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (!document.getElementById("gameoverInstructions").classList.contains("show"))
+                setTime(time + 1);
+            timing.current.innerText = time;
+        }, 1000);
+        return () => clearInterval(interval);
+    }, [time]);
+    useEffect(() => {
+        const script = document.createElement("script");
+        script.src = "/bunny.js";
+        script.async = true;
+
+        document.body.appendChild(script);
+
+        // eslint-disable-next-line react-hooks/exhaustive-deps
+    }, [])
+    const withdrawEarnings = () => {
+        const score = document.getElementById("distValue").textContent;
+        alert(`Achieved ${score} points in ${time} seconds!`)
+    }
+    return (
+        <>
+
+            <div id="world" />
+
+
+            <div id="gameoverInstructions">
+                Game Over<br />
+                <button onClick={withdrawEarnings} className="withdraw">
+                    Withdraw tokens
+                </button>
+            </div>
+            <div id="dist">
+                <div class="label">distance</div>
+                <div id="distValue">000</div>
+                <div id="timing" ref={timing}></div>
+            </div>
+
+
+
+
+            <div id="instructions">Press Space to Jump<span class="lightInstructions"> — Grab the carrots / avoid the hedgehogs</span></div>
+
+
+
+        </>
+    );
+}
+export default BunnyRunner;
